7462ecc72c5fdb97ab37d79b720f3f6e48617a75
Vinicius Coelho <vinicoelhose@gmail.com>
2020-05-24 16:28:36 -0300

    Add lottoBacked
    lottoBacked implements the Combination interface.

---
+++ src/storage/lottostore/lotto_backed.go
@@ -0,0 +1,121 @@
+package lottostore
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sort"
+
+	"github.com/combina/src/db"
+	"github.com/combina/src/storage/types"
+)
+
+type lottoBacked struct {
+	storage map[string]types.Lotto
+}
+
+func NewLottoBacked() (*lottoBacked, error) {
+	lb := &lottoBacked{
+		storage: make(map[string]types.Lotto, 0),
+	}
+
+	err := lb.initializeLottoBacked()
+	if err != nil {
+		return nil, err
+	}
+
+	return lb, nil
+}
+
+func (lb *lottoBacked) initializeLottoBacked() error {
+	conn, err := db.DatabaseConnect(types.DatabaseName)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query(context.Background(), "SELECT * FROM lotto")
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		lotto := types.Lotto{}
+		if err = rows.Scan(&lotto.ID, &lotto.GameType, &lotto.Numbers, &lotto.CreatedOn, &lotto.Alias); err != nil {
+			return err
+		}
+
+		lb.storage[lotto.ID] = lotto
+	}
+
+	return nil
+}
+
+func (lb lottoBacked) ListCombinations(gameType string) ([]types.Lotto, error) {
+	ll := make([]types.Lotto, 0)
+	for _, cur := range lb.storage {
+		if gameType != "" && cur.GameType == gameType {
+			ll = append(ll, cur)
+		}
+	}
+	sort.Slice(ll, func(i, j int) bool {
+		return ll[i].CreatedOn.UnixNano() > ll[j].CreatedOn.UnixNano()
+	})
+
+	return ll, nil
+}
+
+func (lb *lottoBacked) CreateCombination(lotto types.Lotto) error {
+	if _, ok := lb.storage[lotto.ID]; ok {
+		return types.CombinationAlreadyExistsError{}
+	}
+
+	bytes, err := json.Marshal(lotto.Numbers)
+	if err != nil {
+		return fmt.Errorf("marshal failed: %w", err)
+	}
+
+	conn, err := db.DatabaseConnect(types.DatabaseName)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	_, err = conn.Exec(context.Background(), "INSERT INTO lotto (id, type, combination, created_on, name) VALUES ($1, $2, $3, $4, $5)",
+		lotto.ID, lotto.GameType, bytes, lotto.CreatedOn, lotto.Alias)
+	if err != nil {
+		return fmt.Errorf("insert failed: %w", err)
+	}
+
+	lb.storage[lotto.ID] = lotto
+	return nil
+}
+
+func (lb lottoBacked) ReadCombination(id string) (types.Lotto, error) {
+	l, ok := lb.storage[id]
+	if !ok {
+		return types.Lotto{}, types.CombinationDoesNotExistError{}
+	}
+
+	return l, nil
+}
+
+func (lb *lottoBacked) DeleteCombination(id string) error {
+	if _, ok := lb.storage[id]; !ok {
+		return types.CombinationDoesNotExistError{}
+	}
+
+	conn, err := db.DatabaseConnect(types.DatabaseName)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	_, err = conn.Exec(context.Background(), "DELETE FROM lotto WHERE id = $1", id)
+	if err != nil {
+		return fmt.Errorf("deletion failed: %w", err)
+	}
+
+	delete(lb.storage, id)
+	return nil
+}
