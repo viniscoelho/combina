2674335ce6b32f5a4dc5a8cbdd9ca5b56c33a393
Vinicius Coelho <vinicoelhose@gmail.com>
2026-08-31 10:27:36 -0300

    chore: update footer and conferir buttom text

--- common/footer.js
+++ common/footer.js
@@ -2,6 +2,6 @@
 document.write(`
 <div class='menu centered'>
   <div id='copyright'>
-    Combina Jogos &copy; 2020. Vinicius e Amaro Coelho. Todos os direitos reservados.
+    Combina Jogos &copy; 2020-2026. Vinicius e Amaro Coelho. Todos os direitos reservados.
   </div>
 </div>`)
